568c9e4d0c468144ce9caf3cb4261f7bdfdf5c05
syeda zehra <zehrasyeda@gmail.com>
2018-08-23 20:56:28 -0500

    latest

--- public/js/views.js
+++ public/js/views.js
@@ -63,26 +63,130 @@ $(document).ready(function() {
 
     });
 
+// sean's update
+$("#form1").submit(function() {
+  if ($("#input1b").val() !== '') {
+      var newTask1 = $('#input1b').val();
+      var newLi1 = $("<li>" + newTask1 + "</li>" + "<button class='completed'>Completed</button>");
+    
+      $("#body1").prepend(newLi1); // To put the new task at the top of the list
+      $(".input").val('');
+      return false; // So the change persists
+  }
+});
+// $('ul').sortable(); // Because what good is a to-do list that you can't sort? :)
 
-     $('form').submit(function() {
-       if ($('.input').val() !== '') {
-           var newTask = $('.input').val();
-           var newLi = $('<li>' + newTask + '</li>');
-         
-           $('.body').prepend(newLi); // To put the new task at the top of the list
-           $('.input').val('');
-           return false; // So the change persists
-       }
-   });
-   // $('ul').sortable(); // Because what good is a to-do list that you can't sort? :)
+ $("#form2").submit(function() {
+  if ($("#input2b").val() !== '') {
+      var newTask2 = $('#input2b').val();
+      var newLi2 = $("<li>" + newTask2 + "</li>" + "<button class='completed'>Completed</button>");
+    
+      $("#body2").prepend(newLi2); // To put the new task at the top of the list
+      $(".input").val('');
+      return false; // So the change persists
+  }
+});
+
+ $("#form3").submit(function() {
+  if ($("#input3b").val() !== '') {
+      var newTask3 = $('#input3b').val();
+      var newLi3 = $("<li>" + newTask3 + "</li>" + "<button class='completed'>Completed</button>");
+    
+      $("#body3").prepend(newLi3); // To put the new task at the top of the list
+      $(".input").val('');
+      return false; // So the change persists
+  }
+});
+
+$("#form4").submit(function() {
+  if ($("#input4b").val() !== '') {
+      var newTask4 = $('#input4b').val();
+      var newLi4 = $("<li>" + newTask4 + "</li>" + "<button class='completed'>Completed</button>");
+    
+      $("#body4").prepend(newLi4); // To put the new task at the top of the list
+      $(".input").val('');
+      return false; // So the change persists
+  }
+});
+
+$("#form5").submit(function() {
+  if ($("#input5b").val() !== '') {
+      var newTask5 = $('#input5b').val();
+      var newLi5 = $("<li>" + newTask5 + "</li>" + "<button class='completed'>Completed</button>");
+    
+      $("#body5").prepend(newLi5); // To put the new task at the top of the list
+      $(".input").val('');
+      return false; // So the change persists
+  }
+});
+
+});
+
+// set project name
+$("#submit1").click(function(){
+var projectName = $("#project1").val();
+var input = document.getElementById("project1")
+  $("#input1").append("   " + " "+ projectName + "<br>" + "<br>" +"<br>");
+  $("#input1").css("padding", "10px");
+  $("#input1").css("float", "clear");
+  $("#project1").remove();
+  $("#submit1").remove();
+  $("#input_space").append("<br><br>")
 
 
-    $(".project_submit").click(function(){
-      var projectName = $(".project_one").val();
-      var input = document.getElementsByClassName("project_one")
-          $(".timeline-heading").append(projectName);
-          $(".project_one").remove()
-    })
+});
+
+$("#submit2").click(function(){
+var projectName = $("#project2").val();
+var input = document.getElementById("project2")
+  $("#input2").append("   " + " "+ projectName + "<br>" + "<br>" +"<br>");
+  $("#input2").css("padding", "10px");
+  $("#input2").css("float", "clear");
+  $("#project2").remove();
+  $("#submit2").remove();
+  $("#input_space").append("<br><br>")
+
+
+});
+
+$("#submit3").click(function(){
+var projectName = $("#project3").val();
+var input = document.getElementById("project3")
+  $("#input3").append("   " + " "+ projectName + "<br>" + "<br>" +"<br>");
+  $("#input3").css("padding", "10px");
+  $("#input3").css("float", "clear");
+  $("#project3").remove();
+  $("#submit3").remove();
+  $("#input_space").append("<br><br>")
+
+
+});
+
+$("#submit4").click(function(){
+var projectName = $("#project4").val();
+var input = document.getElementById("project4")
+  $("#input4").append("   " + " "+ projectName + "<br>" + "<br>" +"<br>");
+  $("#input4").css("padding", "10px");
+  $("#input4").css("float", "clear");
+  $("#project4").remove();
+  $("#submit4").remove();
+  $("#input_space").append("<br><br>")
+
+
+});
+
+$("#submit5").click(function(){
+var projectName = $("#project5").val();
+var input = document.getElementById("project5")
+  $("#input5").append("   " + " "+ projectName + "<br>" + "<br>" +"<br>");
+  $("#input5").css("padding", "10px");
+  $("#input5").css("float", "clear");
+  $("#project5").remove();
+  $("#submit5").remove();
+  $("#input_space").append("<br><br>")
+
+
+});
 
 });
 
