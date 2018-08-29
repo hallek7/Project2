db0edadd9d286326cfb64cc7d8618f468074d273
syeda zehra <zehrasyeda@gmail.com>
2018-08-28 19:50:01 -0500

    final

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -71,39 +71,18 @@ module.exports = function(app) {
     });
   });
 
-  // app.get("/api/timeline/:id", function(req,res) {
-  //   db.client
-  //     .findAll({
-  //       include: [{
-  //         model: client,
-  //         where: {id: req.params.id }
-  //       }]
-  //     }).then(function(dbclient) {
-  //       res.json(db);
-  //     });
-  // });
+  app.get("/api/timeline/:id", function(req,res) {
+    db.client
+      .findAll({
+        include: [{
+          model: client,
+          where: {id: req.params.id }
+        }]
+      }).then(function(dbclient) {
+        res.json(dbclient);
+      });
+  });
   
  
 };
  
-//   // Get all examples
-//   app.get("/api/examples", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.json(dbExamples);
-//     });
-//   });
-
-//   // Create a new example
-//   app.post("/api/examples", function(req, res) {
-//     db.Example.create(req.body).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-
-//   // Delete an example by id
-//   app.delete("/api/examples/:id", function(req, res) {
-//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-// };
